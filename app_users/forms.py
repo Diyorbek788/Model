9477f8ef43b24286c04354acdad90c16dbc51f04
Diyorbek Baxtiyorov <159403803+Diyorbek788@users.noreply.github.com>
2024-04-21 14:29:10 +0500

    first commit

---
+++ app_users/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+from django.contrib.auth import get_user_model
+from django import forms
+
+User = get_user_model()
+
+
+class UserRegistrationForm(ModelForm):
+    password1 = forms.CharField(max_length=100, widget=forms.PasswordInput())
+    password2 = forms.CharField(max_length=100, widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+class UserForm:
+    def is_valid(self):
+        pass
+
+    def save(self, commit):
+        pass
